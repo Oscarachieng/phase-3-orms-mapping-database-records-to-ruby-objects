52a7a91ad110599d49d07643e58f5dc34b77d0cb
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-24 15:05:44 +0300

    commit tree 8d4919c3d7405f43736f3580be9b676a97cb7f0a

--- lib/song.rb
+++ lib/song.rb
@@ -48,5 +48,22 @@ class Song
     song = Song.new(name: name, album: album)
     song.save
   end
-
+# a class method to convert the returned data from the database
+  def self.new_from_db (row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+  # class method to get all songs from the database
+  def self.all 
+    my_songs = DB[:conn].execute("SELECT * FROM songs")
+    my_songs.map do |song_row|
+      self.new_from_db(song_row)
+    end
+  end
+  #cclass method to find a song by name
+  def self.find_song_by_name (name)
+    my_song = DB[:conn].execute("SELECT * FROM songs WHERE name IS ? LIMIT 1", name)
+    my_song.map do |this_very_song|
+      self.new_from_db(this_very_song)
+    end
+  end
 end
